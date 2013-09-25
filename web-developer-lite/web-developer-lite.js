a973649b76da8b277108f56cf9a37ff6f5057f3f
web-developer-lite <zachborboa+web-developer-lite@gmail.com>
2013-09-24 17:21:33 -0700

    Use inline styles instead of referencing css stylesheet

--- web-developer-lite/web-developer-lite.js
+++ web-developer-lite/web-developer-lite.js
@@ -1,5 +1,10 @@
-var link = document.createElement( 'link' );
-link.setAttribute( 'rel', 'stylesheet' );
-link.setAttribute( 'type', 'text/css' );
-link.setAttribute( 'href', '../css/outline-block-level-elements.css' );
-document.getElementsByTagName( 'head' )[ 0 ].appendChild( link );
+var style = document.createElement( 'style' );
+style.setAttribute( 'type', 'text/css' );
+style.innerHTML = [
+    'address, blockquote, h1, h2, h3, h4, h5, h6, p, pre { outline: 1px solid #b94a48 !important; }',
+    'dd, dl, dt, ol, ul { outline: 1px solid #468847 !important; }',
+    'div, form, table { outline: 1px solid #3a87ad !important; }',
+    ''
+].join( '' );
+
+document.getElementsByTagName( 'head' )[ 0 ].appendChild( style );
